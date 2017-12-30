8c5ecb1342887fcb13b3c6967b50ed43040c40af
lizhengqiang <lizhengqiang@mouge.wang>
2017-12-30 13:24:14 +0800

    Fixed

--- background.js
+++ background.js
@@ -32,7 +32,7 @@ const TransacationLooper = async function () {
 }
 setTimeout(TransacationLooper())
 const sendTransacation = function (wallet, to, num) {
-    num = Number(num.toFiexd(6))
+    num = Number(num.toFixed(6))
     return new Promise((rs, rj) => {
         const from = '0x' + wallet.getAddress().toString('hex')
         console.log("from", from)
