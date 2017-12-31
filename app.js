ddcf0d38b30dfbd37c06050cf0a0627443c23adf
LFZJun <ljun20160606@gmail.com>
2017-12-31 20:31:21 +0800

    feat: 发送时间间隔

--- app.js
+++ app.js
@@ -1,41 +1,51 @@
-chrome.storage.sync.get({
-    "mode": "value",
-    "min": 0.1,
-    "kg": false,
-    "wallet": null,
-    "to_address": null,
-    "password": null,
-}, function (result) {
-    $("#mode").val(result.mode)
-    $("#min").val(result.min)
-    $("#kg").val(result.kg)
-    $("#wallet").val(result.wallet)
-    $("#to_address").val(result.to_address)
-    $("#password").val(result.password)
-})
+const interval = 'interval';
+const mode = 'mode';
+const min = 'min';
+const kg = 'kg';
+const wallet = 'wallet';
+const to_address = 'to_address';
+const password = 'password';
 
-const DoUpdate = function () {
-    console.log("update")
+chrome.storage.sync.get({
+    [interval]: 5,
+    [mode]: "value",
+    [min]: 0.1,
+    [kg]: false,
+    [wallet]: null,
+    [to_address]: null,
+    [password]: null,
+}, function (result) {
+    $("#interval").val(result.interval);
+    $("#mode").val(result.mode);
+    $("#min").val(result.min);
+    $("#kg").val(result.kg);
+    $("#wallet").val(result.wallet);
+    $("#to_address").val(result.to_address);
+    $("#password").val(result.password);
+});
+
+const DoUpdate = () => {
+    console.log("update");
     chrome.storage.sync.set({
-        "mode": $("#mode").val(),
-        "min": $("#min").val(),
-        "kg": $("#kg").val(),
-        "wallet": $("#wallet").val(),
-        "to_address": $("#to_address").val(),
-        "password": $("#password").val(),
+        [interval]: $('#interval').val(),
+        [mode]: $("#mode").val(),
+        [min]: $("#min").val(),
+        [kg]: $("#kg").val(),
+        [wallet]: $("#wallet").val(),
+        [to_address]: $("#to_address").val(),
+        [password]: $("#password").val(),
     }, function (result) {
         console.log(result)
     })
-}
-$("input").change(DoUpdate)
-$("#update").click(DoUpdate)
+};
 
+$("input").change(DoUpdate);
+$("#update").click(DoUpdate);
 $("#like").click(function () {
-    DoUpdate()
+    DoUpdate();
     chrome.extension.sendRequest({
         to_address: "0x1889aea32bebda482440393d470246561a4e6ca6"
     }, function (response) {
         console.log(response);
-
     });
-})
+});
